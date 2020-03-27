f5cb526b291bad9e4b667cc2ff8dcc45696add7d
ilya.ploskovitov@gmail.com <9UIxhVqQBDFU>
2020-03-27 21:49:43 +0300

    add main.py dbHelper.py

---
+++ main.py
@@ -0,0 +1,59 @@
+import logging
+import threading
+import time
+
+import schedule
+import telebot
+
+from telebot import types
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG)
+
+bot = telebot.TeleBot(botToken, threaded=True)
+
+@bot.message_handler(commands=['start'])
+def handleStart(m):
+    pass
+
+@bot.message_handler(content_types= 'text' )
+def simpleTextMessage(m):
+    pass
+
+
+
+def checkNotifications ():
+    pass
+
+def updateNewNotifciations ():
+    pass
+
+
+def threadCheckNotifications():
+    while True:
+        schedule.run_pending()
+        checkNotifications ()
+        time.sleep(1)
+
+
+
+def threadUpdateNotifications():
+    while True:
+        schedule.run_pending()
+        updateNotifications ()
+        time.sleep(1)
+
+
+th1 = threading.Thread(target=threadCheckNotifications, args=())
+th1.start()
+
+
+th2 = threading.Thread(target=threadUpdateNotifications, args=())
+th2.start()
+
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        time.sleep(5)
